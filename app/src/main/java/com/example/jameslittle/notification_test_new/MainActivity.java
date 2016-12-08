884da85e714ddf6c5634d91dcc1debf4bd09998c
Jamescan2 <1506437@my.brunel.ac.uk>
2016-12-08 12:23:36 +0000

    Initial commit

---
+++ app/src/main/java/com/example/jameslittle/notification_test_new/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.jameslittle.notification_test_new;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static com.example.jameslittle.notification_test_new.R.layout.notification;
+
+public class MainActivity extends Activity {
+    Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1 = (Button)findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNotification();
+            }
+        });
+    }
+
+    private void addNotification() {
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.app_icon)
+                        .setContentTitle("BrunelBuySell")
+                        .setContentText("New products are available!");
+        builder.setAutoCancel(true);
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        builder.setContentIntent(contentIntent);
+
+        // Add as notification
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(0, builder.build());
+
+    }
+}
---
+++ app/src/main/java/com/example/jameslittle/notification_test_new/NotificationView.java
@@ -0,0 +1,16 @@
+package com.example.jameslittle.notification_test_new;
+
+/**
+ * Created by James Little on 005 05 Dec 16.
+ */
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class NotificationView extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification);
+    }
+}
